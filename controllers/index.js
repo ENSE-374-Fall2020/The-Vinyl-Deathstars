6fdf203b5d90e2f385589b7912d4e64e5a2c1aba
tristanhannibal <tristanhannibal@gmail.com>
2020-11-14 01:59:59 -0600

    initial commit

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const port = 3000;
+
+app.listen(port, function () {
+    console.log(' server is running ' + port);
+});
+
+app.get('/', function (req, res) {
+    res.render('./pages/postbook');
+});
